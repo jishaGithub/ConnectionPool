5e32bcf42b49bcc7ddc3d5e1eb92d6179d1c8a16
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-18 18:41:35 -0400

    Delete src/main directory

--- src/main/java/com/solvd/laba/threads/CustomRunnable.java
+++
@@ -1,9 +0,0 @@
-package com.solvd.laba.threads;
-
-public class CustomRunnable implements Runnable{
-
-    @Override
-    public void run() {
-        System.out.println("Thread is created using Runnable");
-    }
-}
