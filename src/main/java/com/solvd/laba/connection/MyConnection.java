6a9baffd9d248059371dfcb1dbdb1d581675d8d6
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-18 18:38:32 -0400

    Updated Connection Pool Project

---
+++ src/main/java/com/solvd/laba/connection/MyConnection.java
@@ -0,0 +1,40 @@
+package com.solvd.laba.connection;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.io.IOException;
+import java.net.Socket;
+
+public class MyConnection {
+    private static final Logger logger = LogManager.getLogger(MyConnection.class);
+    private Socket socket;
+    private int idTracker = 0;
+    private int connectionId;
+    public MyConnection() {
+        this.connectionId = ++idTracker;
+        this.socket = new Socket();
+        logger.info("Socket opened");
+    }
+
+    public int getConnectionId() {
+        return connectionId;
+    }
+
+    public void performAuthentication() {
+        logger.info("Authentication performed");
+    }
+
+    public void performTask() {
+        logger.info("Task completed successfully");
+    }
+
+
+    public void closeConnection() {
+        try {
+            socket.close();
+            logger.info("Connection closed");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/solvd/laba/threads/CustomRunnable.java
@@ -0,0 +1,9 @@
+package com.solvd.laba.threads;
+
+public class CustomRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Thread is created using Runnable");
+    }
+}
