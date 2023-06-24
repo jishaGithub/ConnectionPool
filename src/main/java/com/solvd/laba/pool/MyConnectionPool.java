0c6757dcfdb9ba7145579d9b11c96e3bfc3711a3
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-24 13:59:49 -0400

    Add files via upload

--- src/main/java/com/solvd/laba/pool/MyConnectionPool.java
+++ src/main/java/com/solvd/laba/pool/MyConnectionPool.java
@@ -1,5 +1,6 @@
 package com.solvd.laba.pool;
 
+
 import com.solvd.laba.connection.MyConnection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,8 +10,8 @@ import java.util.concurrent.*;
 public class MyConnectionPool {
     private static final Logger logger = LogManager.getLogger(MyConnectionPool.class);
     private final ConcurrentLinkedQueue<MyConnection> connectionPool;
-    static MyConnectionPool connectionPoolObject;
-    int counter=0;
+    private static MyConnectionPool connectionPoolObject;
+    private int counter = 0;
 
     public MyConnectionPool() {
         connectionPool = new ConcurrentLinkedQueue<>();
@@ -24,11 +25,9 @@ public class MyConnectionPool {
     }
 
     public void initializePool(int poolSize) throws IOException, InterruptedException {
-        MyConnection connection = null;
         for (int i=1; i<=poolSize; i++) {
-            connection = new MyConnection(i);
-            connectionPool.offer(connection);
-            logger.info("Connection ID:"+connection.getConnectionID()+" added to the connection pool");
+            connectionPool.offer(new MyConnection(i));
+            logger.info(String.format("Connection ID: %d added to the connection pool",i));
             Thread.sleep(500);
         }
     }
@@ -37,7 +36,7 @@ public class MyConnectionPool {
         counter++;
         MyConnection connection = connectionPool.poll();
         if (connection != null) {
-            logger.info("Request No:"+counter+". Got connection:"+connection.getConnectionID());
+            logger.info(String.format("Request No: %d Got connection: %d",counter,connection.getConnectionID()));
             Thread.sleep(500);
         }
         return connection;
@@ -48,7 +47,6 @@ public class MyConnectionPool {
         logger.info("Connection " + connection.getConnectionID() + " is returned back to the connection pool. Current pool size:"+
                 getConnectionPoolSize());
     }
-    
     public int getConnectionPoolSize() {
         return connectionPool.size();
     }
